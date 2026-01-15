12eb7f08a67a81c676ae50e5008ad549467c2bff
Marie Sauve <marie.sauve@triosstudent.com>
2026-01-15 13:29:40 -0500

    Refactor Nextcloud application configurations and RBAC settings for improved organization

--- nextcloud-app/nextcloud/nextcloud.tf
+++ nextcloud-app/nextcloud/nextcloud.tf
@@ -1,7 +1,7 @@
-resource "cloudflare_record" "nextcloud" {
-  zone_id = var.cloudflare_config.app_zone_id
-  type    = "A"
-  name    = "nextcloud.goeasycare.app"
-  value   = data.kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
-  proxied = true
-}
+# resource "cloudflare_record" "nextcloud" {
+#   zone_id = var.cloudflare_config.app_zone_id
+#   type    = "A"
+#   name    = "nextcloud.goeasycare.app"
+#   value   = data.kubernetes_service.nginx.status[0].load_balancer[0].ingress[0].ip
+#   proxied = true
+# }
