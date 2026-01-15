d7d046080f664f04065b233b764fe69a28d98be8
Marie Sauve <marie.sauve@triosstudent.com>
2026-01-15 15:32:53 -0500

    Remove deprecated Terraform files and update app.yaml

--- nextcloud-app/nextcloud/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  cluster_name     = "development-gke"
-  cluster_location = "northamerica-northeast1-a"
-  project_id       = "gec-development-cluster"
-}
--- nextcloud-app/nextcloud/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_config" {
-  sensitive   = true
-  description = "Cloudflare zone api token"
-  type = object({
-    api_token      = string
-    app_zone_id    = string
-    origin_api_key = string
-  })
-}
